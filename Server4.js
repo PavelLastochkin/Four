5cd0123759b69fc96857b91d422c7cfdae5976e4
PavelLastochkin <89576727+PavelLastochkin@users.noreply.github.com>
2021-12-10 16:26:50 +0300

    Update Server4.js

--- Server4.js
+++ Server4.js
@@ -3,10 +3,6 @@ const app = require('express')()
 const host = '127.0.0.1'
 const port = 4000
 
-app.get('/home', (req, res) => {
-    res.status(200).type('text/plain')
-    res.send('Home page')
-  })
   app.get('/', (req, res) => {
     res.status(200).type('text/plain')
     res.send('Home page')
